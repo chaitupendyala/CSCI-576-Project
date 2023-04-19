889e5ca4e82316a38dca0057286e4ffbfae45698
Phani Chaitanya Pendyala <prndyala@gmail.com>
2023-04-18 21:18:11 -0700

    Done some analysis on audio. Generated a spectral to better understand the change in scenes.

--- create_indexes/analyze_audio.py
+++ create_indexes/analyze_audio.py
@@ -4,26 +4,28 @@ import sklearn
 
 file_name = '../Data_Sets/Ready_Player_One_rgb/InputAudio.wav'
 
-x, sr = librosa.load(file_name)
+'''
+ \*
+  * Load an audio file as a floating point time series.
+  * sr=None makes sure lobrosa does not resample the audio at a different rate
+  * Returns:
+      * y: n dimentional np array, this is the audio time series.
+      * sr: sampling rate
+'''
+x, sr = librosa.load(file_name, sr=None)
+print(type(x), type(sr))
+print("x.shape, sr: ", x.shape, sr)
 
+'''
+  \*
+   * Trying to plot the spetral plot.
+   * According to the spectral plot we can 
+      a lot of entropy difference while a scene changes
+   * This change in entropy can be used to check the scene change
+'''
 X = librosa.stft(x)
-
 Xdb = librosa.amplitude_to_db(abs(X))
-
-# plt.figure(figsize = (10, 5))
-# librosa.display.specshow(Xdb, sr = sr, x_axis = 'time', y_axis = 'hz')
-# plt.colorbar()
-
-spectral_centroids = librosa.feature.spectral_centroid(y=x, sr=sr)[0]
-spectral_centroids.shape(775, )
-# Computing the time variable for visualization
-plt.figure(figsize = (12, 4))
-frames = range(len(spectral_centroids))
-t = librosa.frames_to_time(frames)
-# Normalising the spectral centroid for visualisation
-def normalize(x, axis = 0):
-  return sklearn.preprocessing.minmax_scale(x, axis = axis)
-#Plotting the Spectral Centroid along the waveform
-librosa.display.waveplot(x, sr = sr, alpha = 0.4)
-plt.plot(t, normalize(spectral_centroids), color = 'b')
+plt.figure(figsize=(14, 5))
+librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='hz')
+plt.colorbar()
 plt.show()
