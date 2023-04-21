9a1b9195da83a8ca2612029e1b7135d57655ba5b
Phani Chaitanya Pendyala <prndyala@gmail.com>
2023-04-20 22:09:59 -0700

    Code to generate the entropy change between audio frames. This along with the video data will help us determine the scene change.

--- create_indexes/analyze_audio.py
+++ create_indexes/analyze_audio.py
@@ -1,6 +1,7 @@
 import librosa
+import numpy as np
+from scipy.stats import entropy
 import matplotlib.pyplot as plt
-import sklearn
 
 file_name = '../Data_Sets/Ready_Player_One_rgb/InputAudio.wav'
 
@@ -16,16 +17,34 @@ x, sr = librosa.load(file_name, sr=None)
 print(type(x), type(sr))
 print("x.shape, sr: ", x.shape, sr)
 
-'''
-  \*
-   * Trying to plot the spetral plot.
-   * According to the spectral plot we can 
-      a lot of entropy difference while a scene changes
-   * This change in entropy can be used to check the scene change
-'''
-X = librosa.stft(x)
-Xdb = librosa.amplitude_to_db(abs(X))
-plt.figure(figsize=(14, 5))
-librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='hz')
-plt.colorbar()
+# Flatten the audio signal to later determine the entropy change
+x.flatten()
+
+# Define frame size and hop size in samples
+frame_size = int(0.02 * sr) # 20 ms
+hop_size = int(0.01 * sr) # 10 ms
+
+# Segmenting the audio signal into smaller parts, 20ms in this case
+window = np.hanning(frame_size)
+n_frames = 1 + (len(x) - frame_size) // hop_size
+frames = np.lib.stride_tricks.as_strided(x, shape=(n_frames, frame_size),
+                                         strides=(x.strides[0]*hop_size, x.strides[0]))
+frames = frames * window
+
+
+# Computing power spectrum for each frame using fft
+power_spectrum = np.abs(np.fft.fft(frames))**2
+normalized_spectrum = power_spectrum / np.sum(power_spectrum, axis=1, keepdims=True)
+
+# Computing entropy for each frame
+entropy_values = entropy(normalized_spectrum.T)
+
+# Computing entropy difference between two subsequent frames
+entropy_change = np.abs(np.diff(entropy_values))
+print(entropy_change)
+
+# Plotting the entropy change
+plt.plot(entropy_change)
+plt.xlabel('Frame index')
+plt.ylabel('Entropy change')
 plt.show()
