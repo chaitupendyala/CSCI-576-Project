f857f8250189276c33bdf750c5850da194a7d4ea
Phani Chaitanya Pendyala <prndyala@gmail.com>
2023-04-21 22:37:27 -0700

    Change points are being detected with just audio signal but seem to be wrong.

--- create_indexes/analyze_audio.py
+++ create_indexes/analyze_audio.py
@@ -1,50 +1,51 @@
 import librosa
 import numpy as np
-from scipy.stats import entropy
 import matplotlib.pyplot as plt
 
-file_name = '../Data_Sets/Ready_Player_One_rgb/InputAudio.wav'
+# Split audio into short segments
+def split_audio(audio, sr, segment_length=3):
+    segment_samples = sr * segment_length
+    n_segments = len(audio) // segment_samples
+    return [audio[i * segment_samples : (i + 1) * segment_samples] for i in range(n_segments)]
 
-'''
- \*
-  * Load an audio file as a floating point time series.
-  * sr=None makes sure lobrosa does not resample the audio at a different rate
-  * Returns:
-      * y: n dimentional np array, this is the audio time series.
-      * sr: sampling rate
-'''
-x, sr = librosa.load(file_name, sr=None)
-print(type(x), type(sr))
-print("x.shape, sr: ", x.shape, sr)
+# Compute audio features
+def compute_features(audio, sr):
+    mfccs = librosa.feature.mfcc(y=audio, sr=sr, n_mfcc=13)
+    spectral_contrast = librosa.feature.spectral_contrast(y=audio, sr=sr)
+    spectral_centroid = librosa.feature.spectral_centroid(y=audio, sr=sr)
+    zero_crossing_rate = librosa.feature.zero_crossing_rate(y=audio)
+    return np.concatenate([mfccs, spectral_contrast, spectral_centroid, zero_crossing_rate], axis=0)
 
-# Flatten the audio signal to later determine the entropy change
-x.flatten()
+# Calculate the difference between consecutive features
+def calculate_feature_diff(features):
+    return np.abs(np.diff(features, axis=1))
 
-# Define frame size and hop size in samples
-frame_size = int(0.02 * sr) # 20 ms
-hop_size = int(0.01 * sr) # 10 ms
+# Determine threshold
+def determine_threshold(feature_diff):
+    return np.mean(feature_diff) + np.std(feature_diff)
 
-# Segmenting the audio signal into smaller parts, 20ms in this case
-window = np.hanning(frame_size)
-n_frames = 1 + (len(x) - frame_size) // hop_size
-frames = np.lib.stride_tricks.as_strided(x, shape=(n_frames, frame_size),
-                                         strides=(x.strides[0]*hop_size, x.strides[0]))
-frames = frames * window
+# Detect scene changes
+def detect_scene_changes(audio, sr, threshold):
+    segments = split_audio(audio, sr)
+    segment_features = [compute_features(segment, sr) for segment in segments]
+    feature_diff = calculate_feature_diff(segment_features)
+    change_points = np.argwhere(feature_diff > threshold)[:, 1] + 1
+    return change_points
 
+# Plot audio waveform with scene change lines
+def plot_scene_changes(audio, sr, change_points):
+    plt.figure(figsize=(10, 4))
+    plt.plot(np.arange(len(audio)) / sr, audio)
+    for point in change_points:
+        plt.axvline(x=point * 3, color='r', linestyle='--')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Amplitude')
+    plt.show()
 
-# Computing power spectrum for each frame using fft
-power_spectrum = np.abs(np.fft.fft(frames))**2
-normalized_spectrum = power_spectrum / np.sum(power_spectrum, axis=1, keepdims=True)
-
-# Computing entropy for each frame
-entropy_values = entropy(normalized_spectrum.T)
-
-# Computing entropy difference between two subsequent frames
-entropy_change = np.abs(np.diff(entropy_values))
-print(entropy_change)
-
-# Plotting the entropy change
-plt.plot(entropy_change)
-plt.xlabel('Frame index')
-plt.ylabel('Entropy change')
-plt.show()
+# Example usage
+audio, sr = librosa.load("../Data_Sets/Ready_Player_One_rgb/InputAudio.wav")
+threshold = determine_threshold(calculate_feature_diff(compute_features(audio, sr)))
+change_points = detect_scene_changes(audio, sr, threshold)
+print(change_points)
+print(type(change_points))
+plot_scene_changes(audio, sr, change_points)
