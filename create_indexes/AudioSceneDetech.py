cdce5beb8727c3a3db66d788e7f63b886e8ab7c6
Phani Chaitanya Pendyala <prndyala@gmail.com>
2023-04-27 20:23:06 -0700

    Adding a boilerplate template to plug all detection algorithms we are using.

---
+++ create_indexes/AudioSceneDetech.py
@@ -0,0 +1,3 @@
+class AudioSceneDetech:
+    def __init__(self, audio_file_name=None) -> None:
+        self.audio_file_name = audio_file_name
--- create_indexes/scenedetection.py
+++ create_indexes/scenedetection.py
@@ -1,41 +1,10 @@
-import subprocess
-#import scenedetect
-#pip install scenedetect[opencv] --upgrade
-import cv2
+from .AudioSceneDetech import AudioSceneDetech
+from .VideoSceneDetech import VideoSceneDetech
 
-
-
-print("Generic Scene Detection:")
-# generic scene splitting 
-from scenedetect import detect, ContentDetector
-scene_list = detect('InputVideo.mp4', ContentDetector())
-
-for i, scene in enumerate(scene_list):
-    print('    Scene %2d: Start %s / Frame %d, End %s / Frame %d' % (
-        i+1,
-        scene[0].get_timecode(), scene[0].get_frames(),
-        scene[1].get_timecode(), scene[1].get_frames(),))
-  
-  
-################################################################
-print("Splitting scenes while taking into account fading frames:")
-from scenedetect import detect, ThresholdDetector
-scene_list = detect('InputVideo.mp4', ThresholdDetector())
-  
-for i, scene in enumerate(scene_list):
-    print('    Scene %2d: Start %s / Frame %d, End %s / Frame %d' % (
-        i+1,
-        scene[0].get_timecode(), scene[0].get_frames(),
-        scene[1].get_timecode(), scene[1].get_frames(),))
+class SceneDetech:
+    def __init__(self, audio_file_name=None, video_file_name=None) -> None:
+        self.audio_file_name = audio_file_name
+        self.video_file_name = video_file_name
     
-    
-################################################################    
-print("Splitting scenes while taking into account fast camera movements:")
-from scenedetect import detect, AdaptiveDetector
-scene_list = detect('InputVideo.mp4', AdaptiveDetector())
-  
-for i, scene in enumerate(scene_list):
-    print('    Scene %2d: Start %s / Frame %d, End %s / Frame %d' % (
-        i+1,
-        scene[0].get_timecode(), scene[0].get_frames(),
-        scene[1].get_timecode(), scene[1].get_frames(),))
+    def run_scene_detection(self):
+        pass
