4740a8905428e56f2c192495a0f197818de27f78
Phani Chaitanya Pendyala <prndyala@gmail.com>
2023-05-03 01:04:54 -0700

    Combined video and audio to compute scene, shot and subshot changes.

--- create_indexes/AudioSceneDetech.py
+++ create_indexes/AudioSceneDetech.py
@@ -1,3 +1,36 @@
+import librosa
+import numpy as np
+from scipy.stats import entropy
+from datetime import datetime, timedelta
+
 class AudioSceneDetech:
     def __init__(self, audio_file_name=None) -> None:
         self.audio_file_name = audio_file_name
+        self.audio, self.sr = librosa.load(audio_file_name, sr=None)
+    
+    def _entropy_difference(self, time):
+        time_of_interest = datetime.strptime(time, '%H:%M:%S.%f')
+        unix_epoch = datetime(1900, 1, 1, tzinfo=time_of_interest.tzinfo)
+        time_of_interest_seconds = (time_of_interest - unix_epoch).total_seconds()
+
+        time_index = int(time_of_interest_seconds * self.sr)
+
+        before_index = time_index - (2 * self.sr)
+        after_index = time_index + (2 * self.sr)
+
+        signal_before = self.audio[before_index:time_index]
+        signal_after = self.audio[time_index:after_index]
+
+        entropy_before = entropy(np.abs(signal_before))
+        entropy_after = entropy(np.abs(signal_after))
+
+        entropy_difference = abs(entropy_after - entropy_before)
+
+        return entropy_difference
+    
+    def compute_audio_entropies(self, change_times):
+        entropy_differences = []
+        for change_time in change_times:
+            entropy_differences.append(self._entropy_difference(change_time))
+        
+        return entropy_differences
--- create_indexes/scenedetection.py
+++ create_indexes/scenedetection.py
@@ -1,6 +1,7 @@
 from AudioSceneDetech import AudioSceneDetech
 from VideoSceneDetech import VideoSceneDetech
 from constants import *
+import numpy as np
 
 dataset_locations = {
     READY_PLAYER_ONE : {
@@ -23,17 +24,26 @@ dataset_locations = {
 class SceneDetech:
     def __init__(self, data_set=None) -> None:
         self.data_set = data_set
+        self.videoSceneDetech = None
+        self.audioSceneDetect = None
+    
+    def compute_scene_shot_subshot_thresholds(self, entropies, scene_percentile=80, shot_percentile=10):
+        return (np.percentile(entropies, scene_percentile), np.percentile(entropies, shot_percentile))
+
+    def compute_video_entropies(self, video_change_times):
+        entropy_differences = self.videoSceneDetech.entropy_difference(timestamps=video_change_times, window_size=2)
+        return entropy_differences
 
     def run_scene_detection(self):
         if self.data_set == None or self.data_set not in dataset_locations:
             print("Please provide a dataset")
             return
 
-        videoSceneDetech = VideoSceneDetech(video_file_name = dataset_locations[self.data_set][VIDEO_FILE_MP4])
-        audioSceneDetect = AudioSceneDetech(dataset_locations[self.data_set][AUDIO_FILE])
+        self.videoSceneDetech = VideoSceneDetech(video_file_name = dataset_locations[self.data_set][VIDEO_FILE_MP4])
+        self.audioSceneDetect = AudioSceneDetech(dataset_locations[self.data_set][AUDIO_FILE])
 
-        scene_changes_content_detector = videoSceneDetech.detech_scene_change_using_content_detector()
-        scene_changes_adaptive_detector = videoSceneDetech.detech_scene_change_using_adaptive_detector()
+        scene_changes_content_detector = self.videoSceneDetech.detech_scene_change_using_content_detector()
+        scene_changes_adaptive_detector = self.videoSceneDetech.detech_scene_change_using_adaptive_detector()
 
         scene_change_points = set()
         for scene in scene_changes_content_detector:
@@ -41,17 +51,48 @@ class SceneDetech:
 
         for scene in scene_changes_adaptive_detector:
             scene_change_points.add(scene)
+        
+        scene_change_points = sorted(list(scene_change_points))
 
-        return list(scene_change_points)
+        audio_entropies = self.audioSceneDetect.compute_audio_entropies(scene_change_points)
+        
+        video_entropies = self.compute_video_entropies(video_change_times=scene_change_points)
+
+        video_threshold_scene, video_threshold_shot = self.compute_scene_shot_subshot_thresholds(video_entropies, scene_percentile=80, shot_percentile=10)
+
+        audio_threshold_scene, audio_threshold_shot = self.compute_scene_shot_subshot_thresholds(audio_entropies, scene_percentile=80, shot_percentile=10)
+
+        shots = {"SCENES": [], "SHOTS": [], "SUBSHOTS": []}
+
+        for i in range(len(video_entropies)):
+            current_audio_entropy = audio_entropies[i]
+            current_video_entropy = video_entropies[i]
+            # if current_video_entropy >= video_threshold_scene and current_audio_entropy >= audio_threshold_scene:
+            #     shots["SCENES"].append(scene_change_points[i])
+            # elif (current_video_entropy < video_threshold_scene and current_video_entropy >= video_threshold_shot) and \
+            #      (current_audio_entropy < audio_threshold_shot):
+            #     shots["SHOTS"].append(scene_change_points[i])
+            # else:
+            #     shots["SUBSHOTS"].append(scene_change_points[i])
+            if current_video_entropy >= video_threshold_scene and current_audio_entropy >= audio_threshold_scene:
+                shots["SCENES"].append(scene_change_points[i])
+            elif current_audio_entropy < audio_threshold_scene and current_video_entropy >= video_threshold_scene:
+                shots["SHOTS"].append(scene_change_points[i])
+            # elif current_audio_entropy < audio_threshold_scene and current_video_entropy < video_threshold_scene:
+            #     shots["SUBSHOTS"].append(scene_change_points[i])
+            else:
+                shots["SUBSHOTS"].append(scene_change_points[i])
+
+        return shots
 
 def main():
     sceneDetech = SceneDetech(READY_PLAYER_ONE)
 
-    video_times = sceneDetech.run_scene_detection()
-    print(video_times)
+    scene_changes = sceneDetech.run_scene_detection()
+    print(scene_changes)
 
-    videoSceneDetechController = VideoSceneDetech(video_file_name = dataset_locations[READY_PLAYER_ONE][VIDEO_FILE_MP4])
-    videoSceneDetechController.entropy_difference(timestamps=video_times, window_size=2)
+    # videoSceneDetechController = VideoSceneDetech(video_file_name = dataset_locations[READY_PLAYER_ONE][VIDEO_FILE_MP4])
+    # entropy_differences = videoSceneDetechController.entropy_difference(timestamps=video_times, window_size=2)
 
 if __name__ == '__main__':
     main()
